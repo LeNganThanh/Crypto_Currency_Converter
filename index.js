d3653c9579ebac3dc360ac1b629a449a60d27eda
Thanh Le <99728087+LeNganThanh@users.noreply.github.com>
2022-08-21 10:09:02 +0200

    fetch using async/await

--- index.js
+++ index.js
@@ -4,21 +4,7 @@
 document.body.style.fontSize = "1.3rem";
 const heading = document.querySelector("h1");
 heading.style.margin = "3rem 0";
-
-const convertBtn = document.querySelector(".btn");
-
-const optionsChange = Array.from(document.getElementsByTagName("option"));
-
-convertBtn.addEventListener("click", e => {
-  const crypto = document.getElementById("inputGroupSelect01");
-  const cryptoVal = crypto.options[crypto.selectedIndex].text;
-
-  const currency = document.getElementById("inputGroupSelect02");
-  const currencyVal = currency.options[currency.selectedIndex].text;
-
-  const cryptoAmt = document.querySelector(".crypto-amt").value;
-  const currencyAmt = document.querySelector(".currency-amt");
-
+/* 
   const currencyConvert = `https://api.coinbase.com/v2/prices/${cryptoVal}-${currencyVal}/spot`;
   fetch(currencyConvert)
     .then(response => {
@@ -35,4 +21,40 @@ convertBtn.addEventListener("click", e => {
       alert("Invalid base currency!");
       location.reload();
     });
+*/
+
+//async function to get fetch data from coinbase
+async function getData(cry, curr) {
+  const currencyConvertUrl = `https://api.coinbase.com/v2/prices/${cry}-${curr}/spot`;
+  let responseVal;
+  try {
+    const getVal = await fetch(currencyConvertUrl);
+    const response = await getVal.json();
+    responseVal = response.data.amount;
+  } catch (error) {
+    alert(error);
+    location.reload();
+  }
+  console.log(responseVal);
+  return responseVal;
+}
+
+const convertBtn = document.querySelector(".btn");
+
+//catch the "click" event of the button
+convertBtn.addEventListener("click", e => {
+  const crypto = document.getElementById("inputGroupSelect01");
+  const cryptoVal = crypto.options[crypto.selectedIndex].text;
+
+  const currency = document.getElementById("inputGroupSelect02");
+  const currencyVal = currency.options[currency.selectedIndex].text;
+
+  const cryptoAmt = document.querySelector(".crypto-amt").value;
+  const currencyAmt = document.querySelector(".currency-amt");
+
+  //get data from async function
+  getData(cryptoVal, currencyVal).then(result => {
+    const amountData = +result;
+    currencyAmt.value = amountData * cryptoAmt;
+  });
 });
